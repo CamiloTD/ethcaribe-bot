ff4490b7848e4c262b28ebb096cc8fd30caa06cb
CamiloTD <camilotd1999@gmail.com>
2020-08-14 15:30:04 -0500

    Add Hot Reloading and First Command

--- index.ts
+++ index.ts
@@ -1,19 +1,24 @@
 import TelegramBot from 'node-telegram-bot-api';
 import dotenv from "dotenv";
+import { log, highlight, cold } from "termx";
 
 dotenv.config();
 
 const { BOT_TOKEN } = process.env;
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
-bot.onText(/\/(.+?) (.+)/, async function (msg, match) {
+bot.onText(/^\/(.+?)(?: (.+))?$/, async function (msg, match) {
     const [, cmd, params] = match;
-    const args = params.split(" ");
+    const args = (params || "").split(" ");
 
-    if(!Commands[cmd]) var response = await Commands.$notFound(msg, ...args);
+    log(`${highlight("COMMAND")} - <${msg.from.id}> ${cold(msg.from.first_name + " " + msg.from.last_name + ":")} /${cmd} ${params}`);
+
+    if(!Commands[cmd]) var response = await Commands.$notFound(msg, cmd);
     else var response = await Commands[cmd](msg, ...args);
 
     if(typeof response === "string") bot.sendMessage(msg.chat.id, response as unknown as string);
 });
+
+
 
 import * as Commands from './src/commands';
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,7 @@
-export function $notFound (msg, ...args) {
-    
+export function $notFound (msg, command: string) {
+    return `Command not found: ${command}`;
+}
+
+export function register (msg, address: string) {
+    return `Address registered: ${address}`;
 }
